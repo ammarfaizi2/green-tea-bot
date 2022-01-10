ammarfaizi2/green-tea-bot

commit 7dadb74734733ce8aaf3d85d506f8471083e1412

web/api: Add get_msg_count_group API for stats and sync
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

web/api/src/classes/GreenTea/API/GetMessageCountGroup.php
@@ -1,0 +1,101 @@
+<?php
+/* SPDX-License-Identifer: GPL-2.0-only */
+/*
+ * Copyright (C) 2021  Ammar Faizi <ammarfaizi2@gmail.com>
+ */
+
+namespace GreenTea\API;
+
+use PDO;
+use GreenTea\APIFoundation;
+
+class GetMessageCountGroup extends APIFoundation
+{
+	/**
+	 * @return array
+	 */
+	public function getCount(): array
+	{
+		$pdo = $this->getPDO();
+		$date = date("Y-m-d");
+		$query = <<<SQL
+			SELECT
+			gt_groups.name, COUNT(1) AS msg_count
+			FROM gt_messages
+			INNER JOIN gt_message_content
+			ON gt_messages.id = gt_message_content.id
+			INNER JOIN gt_groups
+			ON gt_groups.id = gt_messages.chat_id WHERE
+			gt_message_content.tg_date >= '{$date} 00:00:00' AND
+			gt_message_content.tg_date <= '{$date} 23:59:59'
+			GROUP BY gt_messages.chat_id
+			ORDER BY msg_count DESC;
+SQL;
+		$st = $pdo->prepare($query);
+		$st->execute();
+		$this->errorCode = 0;
+		return [
+			"is_ok" => true,
+			"msg"   => NULL,
+			"data"  => $st->fetchAll(PDO::FETCH_ASSOC),
+		];
+	}
+
+	public function getCountStats(string $startDate, ?string $endDate = NULL): array
+	{
+		$data = [];
+
+		if ($endDate) {
+			$epEndDate = strtotime($endDate);
+			$endDate = date("Y-m-d", $epEndDate)." 23:59:59";
+		} else {
+			$endDate = date("Y-m-d")." 23:59:59";
+			$epEndDate = time();
+		}
+
+		$origEpStartDate = strtotime($startDate);
+		$startDate = date("Y-m-d", $origEpStartDate)." 00:00:00";
+		$epStartDate = $origEpStartDate;
+		while ($epStartDate <= $epEndDate) {
+			$data[date("Y-m-d", $epStartDate)] = 0;
+			$epStartDate += 3600 * 24;
+		}
+		var_dump($startDate, $endDate);
+		$query = <<<SQL
+			SELECT
+			DATE(tg_date) AS msg_date, COUNT(1) AS nr_msg
+			FROM gt_message_content
+			WHERE tg_date >= ? AND tg_date <= ?
+			GROUP BY msg_date
+			ORDER BY msg_date DESC
+SQL;
+		$pdo = $this->getPDO();
+		$st = $pdo->prepare($query);
+		$st->execute([$startDate, $endDate]);
+		$this->errorCode = 0;
+		while ($r = $st->fetch(PDO::FETCH_ASSOC))
+			$data[$r["msg_date"]] = (int) $r["nr_msg"];
+
+		return [
+			"is_ok" => true,
+			"msg"   => NULL,
+			"data"  => $data,
+		];
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getErrorCode(): int
+	{
+		return $this->errorCode;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isError(): bool
+	{
+		return $this->errorCode != 0;
+	}
+};

web/api/public/api.php
@@ -10,6 +10,7 @@ use GreenTea\API\Login;
 use GreenTea\API\GetGroupList;
 use GreenTea\API\GetChatMessages;
 use GreenTea\API\RegisterAccount;
+use GreenTea\API\GetMessageCountGroup;
 
 if (isset($_SERVER["HTTP_ORIGIN"]) && is_string($_SERVER["HTTP_ORIGIN"])) {
 	header("Access-Control-Allow-Origin: {$_SERVER["HTTP_ORIGIN"]}");
@@ -99,6 +100,28 @@ try {
 
 		$api = new Login();
 		$msg = $api->doLogin($_POST["email"], $_POST["pass"]);
+		if ($api->isError())
+			$code = $api->getErrorCode();
+		break;
+	case "get_msg_count_group":
+		$api = new GetMessageCountGroup();
+
+		if (isset($_GET["stats"])) {
+			if (!isset($_GET["start_date"]) || !is_string($_GET["start_date"])) {
+				$msg  = "Missing \"start_date\" string data";
+				$code = 400;
+				goto out;
+			}
+
+			if (!isset($_GET["end_date"]) || !is_string($_GET["end_date"]))
+				$_GET["end_date"] = date("Y-m-d");
+
+			$msg = $api->getCountStats($_GET["start_date"],
+						   $_GET["end_date"]);
+		} else {
+			$msg = $api->getCount();
+		}
+
 		if ($api->isError())
 			$code = $api->getErrorCode();
 		break;

web/api/src/classes/GreenTea/API/Login.php
@@ -60,15 +60,17 @@ class Login extends APIFoundation
 		if (isset($r["password"]) && password_verify($pass, $r["password"])) {
 			$isOk  = true;
 			$token = $this->createSession($pdo, (int) $r["id"]);
-			$msg   = ["token" => $token];
+			$msg   = "Login success!";
 		} else {
+			$token = NULL;
 			$isOk  = false;
-			$msg   = ["msg" => "Login failed!"];
+			$msg   = "Login failed!";
 		}
 
 		return [
 			"is_ok" => $isOk,
 			"msg"   => $msg,
+			"token" => $token,
 			"data"  => $data,
 		];
 	}

commit fd95c6755e573f24386b456901ea980229266945

web/api: Remove unused var_dump
Fixes: 7dadb74734733ce8aaf3d85d506f8471083e1412 ("web/api: Add get_msg_count_group API for stats and sync")
Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

web/api/src/classes/GreenTea/API/GetMessageCountGroup.php
@@ -60,7 +60,7 @@ SQL;
 			$data[date("Y-m-d", $epStartDate)] = 0;
 			$epStartDate += 3600 * 24;
 		}
-		var_dump($startDate, $endDate);
+
 		$query = <<<SQL
 			SELECT
 			DATE(tg_date) AS msg_date, COUNT(1) AS nr_msg